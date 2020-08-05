b84ae63cb2828c4f670e60301167d5edb01099b2
ameeno <fastah@gmail.com>
2020-08-05 15:27:15 +0100

    Revert "Updated the extension and made some visual changes. updated jquery. to 3.5.1"
    This reverts commit 33ba10eff518e2e17d31f2e762f220ca6c0849e8.

--- Package/README.md
+++ Package/README.md
@@ -1,9 +1,9 @@
 # eBay™ Popularity Sort
 
-A FireFox extension that sorts the eBay™ search results by popularity (number of times sold).
+A firefox extension that sorts the eBay™ search results by popularity (number of times sold).
 
 > Note: This extension is not officially endorsed by eBay™.
-> This extension has been updated to unmodified jQuery 3.3.1 as per FireFox request.
+> This extension has been updated to unmodified Jquery 3.3.1 as per firefox request.
 
 # Preview
 
@@ -11,13 +11,13 @@ A FireFox extension that sorts the eBay™ search results by popularity (number 
 
 # Usage
 
-1. Install the extension from FireFox web store
-2. Search eBay for something ([Edison bulb](http://www.ebay.co.uk/sch/i.html?LH_BIN=1&_from=R40&_sacat=0&_sop=12&_nkw=edison+bulb&_ipg=200&rt=nc)) and make sure `Buy It Now` and `Best Match` are selected
+1. Install the extension from Firefox web store
+2. Search eBay for something ([edison bulb](http://www.ebay.com/sch/i.html?LH_BIN=1&_from=R40&_sacat=0&_sop=12&_nkw=edison+bulb&_ipg=200&rt=nc)) and make sure `Buy It Now` and `Best Match` are selected
 3. Scroll down to bottom of page and set `Items per page` to `200` for this to work better
 
 Enjoy search results sorted by popularity!
 
-I have forked and made changes to allow compatibility with FireFox.
+I have forked and made changes to allow compatibility with firefox.
 
 ## License
 
--- Package/assets/js/content.js
+++ Package/assets/js/content.js
@@ -1,78 +1,65 @@
 // Check whether this is an eBay search results page
-if (
-  window.location.host.startsWith("www.ebay") &&
-  window.location.pathname.startsWith("/sch/")
-) {
-  // Wait for document to load
-  $(document).ready(function () {
-    // Get on/off toggle value for this feature
-    chrome.storage.sync.get(
-      {
-        enableSortByPopularity: true,
-      },
-      function (options) {
-        // Check whether this feature is disabled
-        if (!options.enableSortByPopularity) {
-          return;
-        }
-
-        // Array of search results
-        var results = [];
-
-        // Traverse search results
-        $("ul.srp-results li.s-item, ul#ListViewInner li[listingid]").each(
-          function () {
-            // Convert to jQuery object
-            var listing = $(this);
-
-            // Default listing sold count to 0
-            var soldCount = 0;
-
-            // Extract hotness text
-            var hotnessText = listing
-              .find(".s-item__itemHotness, .hotness-signal")
-              .text()
-              .replace(/,/g, "");
-
-            // Get sold count as integer
-            soldCount = parseInt(hotnessText) || 0;
-
-            // Count indicates number of users watching this item and not number of times sold?
-            if (hotnessText.includes("watching")) {
-              soldCount = 0;
+if (window.location.host.startsWith('www.ebay') && window.location.pathname.startsWith('/sch/')) {
+    // Wait for document to load
+    $(document).ready(function () {
+        // Get on/off toggle value for this feature
+        chrome.storage.sync.get({
+            enableSortByPopularity: true
+        }, function (options) {
+            // Check whether this feature is disabled
+            if (!options.enableSortByPopularity) {
+                return;
             }
 
-            // Count indicates a percentage discount and not number of times sold?
-            if (hotnessText.includes("%")) {
-              soldCount = 0;
-            }
+            // Array of search results
+            var results = [];
 
-            // Add item sold count and listing itself
-            results.push({ sold: soldCount, listing: listing });
+            // Traverse search results
+            $('ul.srp-results li.s-item, ul#ListViewInner li[listingid]').each(function () {
+                // Convert to jQuery object
+                var listing = $(this);
 
-            // Delete element temporarily
-            listing.remove();
-          }
-        );
+                // Default listing sold count to 0
+                var soldCount = 0;
 
-        // Sort all listings by sold count DESC
-        results.sort(function (a, b) {
-          return b.sold - a.sold;
+                // Extract hotness text
+                var hotnessText = listing.find('.s-item__itemHotness, .hotness-signal').text().replace(/,/g, '');
+
+                // Get sold count as integer
+                soldCount = parseInt(hotnessText) || 0;
+
+                // Count indicates number of users watching this item and not number of times sold?
+                if (hotnessText.includes('watching')) {
+                    soldCount = 0;
+                }
+
+                // Count indicates a percentage discount and not number of times sold?
+                if (hotnessText.includes('%')) {
+                    soldCount = 0;
+                }
+
+                // Add item sold count and listing itself
+                results.push({ sold: soldCount, listing: listing });
+
+                // Delete element temporarily
+                listing.remove();
+            });
+
+            // Sort all listings by sold count DESC
+            results.sort(function (a, b) {
+                return b.sold - a.sold;
+            });
+
+            // Get search results parent list
+            var ul = $('.srp-river-answer, #ListViewInner').first();
+
+            // Warn the user about the modified result order
+            ul.append('<p>These search results have been modified by <b>eBay™ Popularity Sort</b>.</p>');
+
+            // Re-add the sorted results
+            results.forEach(function (item) {
+                ul.append(item.listing);
+            });
         });
-
-        // Get search results parent list
-        var ul = $(".srp-river-answer, #ListViewInner").first();
-
-        // Warn the user about the modified result order
-        ul.append(
-          "<p>These search results have been modified by <b>eBay™ Popularity Sort for FireFox</b>.</p>"
-        );
-
-        // Re-add the sorted results
-        results.forEach(function (item) {
-          ul.append(item.listing);
-        });
-      }
-    );
-  });
+    });
 }
